faa174bf5ef15425e432f6b54a112a99ce29f46d
Anh Quach <anh.quachquituan@halliburton.com>
2021-08-10 19:07:23 +0700

    Init code

---
+++ main.tf
@@ -0,0 +1,103 @@
+
+resource "azurerm_resource_group" "vm_rg" {
+  name = var.resource_group_name
+  location = var.location
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_public_ip" "pip" {
+  count = var.is_public ? 1 : 0
+  name = "${var.vm_name}-publicip"
+  resource_group_name = var.resource_group_name
+  location = var.location
+  allocation_method = var.pip_allocation_method
+  sku = var.pip_sku
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name = "${var.vm_name}-nic"
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  location = var.location
+  internal_dns_name_label = "${var.vm_name}"
+  enable_accelerated_networking = var.enable_accelerated_networking
+
+  ip_configuration {
+    name = "${var.vm_name}-ipconf"
+    subnet_id = var.subnet_id
+    private_ip_address_allocation = var.private_ip_address_allocation
+    public_ip_address_id = var.is_public ? azurerm_public_ip.pip[0].id : null
+  }
+
+  lifecycle {
+    ignore_changes = [tags]
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name = "${var.vm_name}-vm"
+  resource_group_name = azurerm_resource_group.vm_rg.name
+  location = var.location
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size = var.vm_size
+  delete_os_disk_on_termination = true
+  delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    id = var.os_image_id
+    publisher = var.os_image_id == "" ? var.os_image_publisher : ""
+    offer = var.os_image_id == "" ? var.os_image_offer : ""
+    sku = var.os_image_id == "" ? var.os_image_sku : ""
+    version = var.os_image_id == "" ? var.os_image_version : ""
+  }
+
+  storage_os_disk {
+    name = "${var.vm_name}-osdisk"
+    create_option = "FromImage"
+    caching = "ReadWrite"
+    managed_disk_type = var.os_disk_type
+  }
+
+  os_profile {
+    computer_name = "${var.vm_name}"
+    admin_username = var.admin_username
+    admin_password = var.os_type == "windows" ? var.admin_password : null
+  }
+
+  dynamic os_profile_linux_config {
+    for_each = var.os_type == "linux" ? [var.os_profile] : []
+    content {
+      disable_password_authentication = true
+      ssh_keys {
+        path = "/home/${var.admin_username}/.ssh/authorized_keys"
+        key_data = var.ssh_public_key
+      }
+    }
+  }
+
+  dynamic os_profile_windows_config {
+    for_each = var.os_type == "windows" ? [var.os_profile] : []
+    content {
+      provision_vm_agent = true
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [tags,storage_image_reference]
+  }
+}
+
+# Dynamic public ip address will be got after it's assigned to a vm
+# Use to output the public IP of the VM
+data "azurerm_public_ip" "pip" {
+  count = length(azurerm_public_ip.pip)
+  name = azurerm_public_ip.pip[count.index].name
+  resource_group_name = var.resource_group_name
+  depends_on = [azurerm_virtual_machine.vm]
+}
---
+++ variable.tf
@@ -0,0 +1,90 @@
+variable "vm_name" {
+  description = "Name of the VM"
+}
+
+variable "resource_group_name" {
+  description = "Resource group contains the VM"
+}
+
+variable "location" {
+  description = "Location of the VM"
+}
+
+variable "subnet_id" {
+  description = "Subnet of the VM"
+}
+
+variable "enable_accelerated_networking" {
+  description = "Enable accelerated networking or not"
+  type        = bool
+  default     = false
+}
+
+variable "private_ip_address_allocation" {
+  description = "Type of address allocation"
+  default     = "Dynamic"
+}
+
+variable "vm_size" {
+  description = "Default size of the VM"
+  default     = "Standard_B2s"
+}
+
+variable "os_image_publisher" {
+  description = "Publisher of the OS image"
+  default     = ""
+}
+
+variable "os_image_offer" {
+  description = "Offer of the OS image"
+  default     = ""
+}
+
+variable "os_image_sku" {
+  description = "SKU of the OS image"
+  default     = ""
+}
+
+variable "os_image_version" {
+  description = "Version of the OS image"
+  default     = ""
+}
+
+variable "os_disk_type" {
+  description = "Defines the type of storage account to be created. Valid options are Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS."
+  default     = "Standard_LRS"
+}
+
+variable "admin_username" {
+  description = "Admin username of the VM"
+}
+
+variable "admin_password" {
+  description = "Admin password of the Windows VM"
+  default = ""
+}
+
+variable "ssh_public_key" {
+  description = "Public key for SSH"
+  default = ""
+}
+
+variable "os_type" {
+  description = "Type of the OS"
+}
+
+variable "is_public" {
+  description = "Define if the VM is public or not"
+  type = bool
+  default = false
+}
+
+variable "pip_allocation_method" {
+  description = "Allocation method for public ip"
+  default = "Static"
+}
+
+variable "pip_sku" {
+  description = "Defines the SKU of the Public IP. Accepted values are Basic and Standard. Defaults to Basic."
+  default     = "Basic"
+}
