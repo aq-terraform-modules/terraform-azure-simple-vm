1951bd18eeb748cfe8babd0b9c6616038ea78d7e
Anh Quach (Tony) <anh.quachquituan@halliburton.com>
2021-09-16 13:10:21 +0700

    Update location to variable

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "azurerm_resource_group" "rg" {
   count    = var.create_rg ? 1 : 0
   name     = var.resource_group_name
-  location = local.location
+  location = var.location
 
   lifecycle {
     ignore_changes = [tags]
